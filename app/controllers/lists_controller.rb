9e6745597b85d1e40a99dc835b454ab12a5b1c17
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-10 16:23:07 -0500

    refactored list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,11 +7,6 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     list_id = @list.id
     @bookmarks = Bookmark.select { |bookmark| bookmark.list_id == list_id }
-    @movies = []
-    @bookmarks.each do |bookmark|
-      @movies.push(Movie.select { |movie| movie.id == bookmark.movie_id })
-    end
-    # raise
   end
 
   def new
