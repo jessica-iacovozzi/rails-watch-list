3f651607733a839287bba35d1764cb01e4c78dfe
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-10 13:24:11 -0500

    Routing, Controller, Views for Lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'lists', to: 'lists#index'
+  get 'lists/new', to: 'lists#new'
+  get 'lists/:id', to: 'lists#show', as: :list
+  post 'lists', to: 'lists#create'
 end
