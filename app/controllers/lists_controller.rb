988b2343b49e3dbf41c6ea2a1aeffe5e09378811
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-10 15:32:22 -0500

    fixed controller and started views for bookmarks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,13 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    list_id = @list.id
+    @bookmarks = Bookmark.select { |bookmark| bookmark.list_id == list_id }
+    @movies = []
+    @bookmarks.each do |bookmark|
+      @movies.push(Movie.select { |movie| movie.id == bookmark.movie_id })
+    end
+    # raise
   end
 
   def new
